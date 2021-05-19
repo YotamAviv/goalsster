fc435f0d3aaf303deaea6790ad2461466f692994
YotamAviv <yotam@aviv.net>
2021-05-19 09:48:54 -0700

    ...

--- Goalsster.py
+++ Goalsster.py
@@ -1,8 +1,6 @@
-import numpy
 import datetime
 import os
-
-from numpy import ndarray
+import numpy
 
 
 # NEXT STEPS:
@@ -22,18 +20,17 @@ def screen_clear():
         _ = os.system('cls')
 
 
-# TODO: BUG: This DayFactory business is not working correctly. UI Should run in real time.
 class RealDayFactory:
-    def get_day(self):
+    def get_today(self):
         days = (datetime.datetime.utcnow() - datetime.datetime(2021, 1, 1)).days
         return days
 
 
 class StubDayFactory:
-    def set_day(self, day):
+    def set_today(self, day):
         self.day = day
 
-    def get_day(self):
+    def get_today(self):
         return self.day
 
 
@@ -51,8 +48,9 @@ MAX_HISTORY = 1000  # should work until 1000 days after 1/1/2021
 
 
 class Goalsster:
-    def __init__(self, day_factory):
-        self.day_factory = day_factory
+    DAY_FACTORY = None
+
+    def __init__(self):
         self.goals = []
 
     def add(self, goal):
@@ -71,6 +69,8 @@ class Goalsster:
         while True:
             screen_clear()
 
+            print("Today is: {}".format(Goalsster.DAY_FACTORY.get_today()))
+
             sorted_goals = sorted(self.goals, key=sortfunc)
             index = 0
             for goal in sorted_goals:
@@ -87,10 +87,9 @@ class Goalsster:
 
 
 class Goal:
-    def __init__(self, day_factory, name, details, spec):
+    def __init__(self, name, details, spec):
         history = numpy.zeros(MAX_HISTORY, dtype=int)
 
-        self.day_factory = day_factory
         self.spec = spec
         self.name = name
         self.details = details
@@ -103,16 +102,13 @@ class Goal:
     #     return 1 + (spec.goal - 1) * (spec.rest + 1)
     #
 
-    # return the number of days since 1/1/2021
-    def get_today(self):
-        return self.day_factory.get_day()
-
     def get_history(self, day):
         return self.history[day]
 
     def make(self):
-        made = self.history[self.get_today()]
-        self.history[self.get_today()] = made + 1
+        today = Goalsster.DAY_FACTORY.get_today()
+        made = self.history[today]
+        self.history[today] = made + 1
 
     def __str__(self):
         failing = ""
@@ -120,13 +116,12 @@ class Goal:
             failing = "FAILING "
         return "{}{:.2f} {} \"{}\" ({})".format(failing, self.score(), self.name, self.details, self.spec)
 
-    # TODO: Cache score
     def score(self):
-        spec = self.spec;
+        spec = self.spec
         history = self.history
 
         score = 0
-        today = self.get_today()
+        today = Goalsster.DAY_FACTORY.get_today()
         hits = 0
         for i in range(0, spec.period):
             day = today - i
@@ -142,7 +137,7 @@ class Goal:
 
                 day_score = day_work_reward / spec.goal
                 score += day_score
-                if (hits >= spec.goal):
+                if hits >= spec.goal:
                     return score
 
         return score
@@ -151,35 +146,35 @@ class Goal:
     def is_failing(self):
         total = 0
         for i in range(0, self.spec.period):
-            day = self.get_today() - i
+            day = Goalsster.DAY_FACTORY.get_today() - i
             done = self.get_history(day)
             total += done
         return total < self.spec.goal
 
 
 def test2():
-    day_factory = StubDayFactory()
-    day_factory.set_day(100)
+    Goalsster.DAY_FACTORY = StubDayFactory()
+    Goalsster.DAY_FACTORY.set_today(100)
 
-    goalsster = Goalsster(day_factory)
+    goalsster = Goalsster()
 
-    run = Goal(day_factory, "run", "Run.", Spec(2, 10))
+    run = Goal("run", "Run.", Spec(2, 10))
     goalsster.add(run)
 
-    pushups = Goal(day_factory, "push-ups", "Do 100 push ups, 2 minute breaks", Spec(2, 6))
+    pushups = Goal("push-ups", "Do 100 push ups, 2 minute breaks", Spec(2, 6))
     goalsster.add(pushups)
     pushups.make()
     # do pushups yesterday
-    day_factory.set_day(99)
+    Goalsster.DAY_FACTORY.set_today(99)
     pushups.make()
     # do pushups day before yesterday
-    day_factory.set_day(98)
+    Goalsster.DAY_FACTORY.set_today(98)
     pushups.make()
-    day_factory.set_day(100)
+    Goalsster.DAY_FACTORY.set_today(100)
 
-    goalsster.add(Goal(day_factory, "word of the week", "Groovy", Spec(5, 10)))
+    goalsster.add(Goal("word of the week", "Groovy", Spec(5, 10)))
 
-    swim = Goal(day_factory, "swim", "Swim.", Spec(3, 365))
+    swim = Goal("swim", "Swim.", Spec(3, 365))
     goalsster.add(swim)
     swim.make()
     swim.make()
@@ -191,18 +186,18 @@ test2()
 
 
 def runUI():
-    day_factory = RealDayFactory()
+    Goalsster.DAY_FACTORY = RealDayFactory()
 
-    goalsster = Goalsster(day_factory)
-    run = Goal(day_factory, "run", "Run.", Spec(2, 10))
+    goalsster = Goalsster()
+    run = Goal("run", "Run.", Spec(2, 10))
     goalsster.add(run)
 
-    pushups = Goal(day_factory, "push-ups", "Do 100 push ups, 2 minute breaks", Spec(2, 6))
+    pushups = Goal("push-ups", "Do 100 push ups, 2 minute breaks", Spec(2, 6))
     goalsster.add(pushups)
 
-    goalsster.add(Goal(day_factory, "word of the week", "Groovy", Spec(5, 10)))
+    goalsster.add(Goal("word of the week", "Groovy", Spec(5, 10)))
 
-    swim = Goal(day_factory, "swim", "Swim.", Spec(3, 365))
+    swim = Goal("swim", "Swim.", Spec(3, 365))
     goalsster.add(swim)
 
     goalsster.ui()
